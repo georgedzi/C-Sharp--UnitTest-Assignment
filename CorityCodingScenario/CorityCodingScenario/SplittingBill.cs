819c9cdee5d457564827fee43a0660d25be9e7ff
georgedzi <georgedziouba@gmail.com>
2017-10-01 15:37:10 -0400

    Add files via upload

---
+++ CorityCodingScenario/CorityCodingScenario/SplittingBill.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+/// <summary>
+/// This namespace consists of one class that is meant to take a file, and calculate total split cost of an activity
+/// </summary>
+namespace CorityCodingScenario
+{
+    /// <summary>
+    /// This class is where the Main method is located. The code execution starts with this class
+    /// It consists of two other methods aside from the Main method (ReadInput, WriteOutput)
+    /// ReadInput reads the content of a txt file, and calculates bill splitting  according to the amount spent on a bill
+    /// WriteInput writes the content provided in ReadInput to a txt.out file
+    /// </summary>
+    public class SplittingBill
+    {
+        /// <summary>
+        /// Initiates the code by passing the txt file directory to the ReadInput method within the object "start"
+        /// "start" is an object of this class  
+        /// </summary>
+        /// <param name="args">For passing command line parameters</param>
+        static void Main(string[] args)
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses.txt");
+            SplittingBill start = new SplittingBill();
+            start.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This method takes a txt file with groups expense information
+        /// It calculates how much one has spent on an activity, then determines if someone is owed money or owes money
+        /// These results are achieved by taking the total spending money, dividing it by the amount of people in the activity, then subtracting the divided amount by the persons spending amount
+        /// </summary>
+        /// <param name="fileName">The directory path of the file that contains the bill spliting information</param>
+        public void ReadInput(string fileName)
+        {
+            string line;
+
+            List<List<decimal>> sectionTotals = new List<List<decimal>>();
+
+
+            StreamReader file = new StreamReader(fileName);
+
+
+            while (!(line = file.ReadLine()).Equals("0"))
+            {
+                int numOfPeople = Int32.Parse(line);
+                decimal paymentTotal = 0;
+                List<decimal> personTotals = new List<decimal>();
+
+                for (int i = 0; i < numOfPeople; i++)
+                {
+
+                    int numOfPayments = Int32.Parse(file.ReadLine());
+
+                    personTotals.Add(0);
+
+                    for (int j = 0; j < numOfPayments; j++)
+                    {
+                        decimal payValue = decimal.Parse(file.ReadLine());
+                        payValue = Math.Round(payValue, 2);
+                        personTotals[i] += payValue;
+                        paymentTotal += payValue;
+                    }
+                }
+
+                for (int k = 0; k < personTotals.Count(); k++)
+                {
+                    personTotals[k] = (paymentTotal / numOfPeople) - personTotals[k];
+                }
+
+                sectionTotals.Add(personTotals);
+
+            }
+            WriteOutput(sectionTotals, fileName);
+
+        }
+
+        /// <summary>
+        /// This method takes any information provided from ReadInput and outputs it to a txt.out file
+        /// Negative amounts are surrounded in brackets, while positive ones are not
+        /// If an amount is positive it means that person owes the group money
+        /// If an amount is negative it means that person is owed money
+        /// </summary>
+        /// <param name="sectionTotals">A list of a List of decimals. This contains information on how much one owes the group, or how much one is owed</param>
+        /// <param name="fileName">The directory path of the file that contains the bill splitting information</param>
+        public void WriteOutput(List<List<decimal>> sectionTotals, string fileName)
+        {
+
+            if (File.Exists((fileName + ".out")))
+            {
+                File.Delete((fileName + ".out"));
+            }
+            List<String> fileOutput = new List<String>();
+            int lineCounter = 0;
+
+            for (int i = 0; i < sectionTotals.Count(); i++)
+            {
+
+                for (int j = 0; j < sectionTotals[i].Count; j++)
+                {
+                    if (sectionTotals[i][j] < 0)
+                    {
+                        sectionTotals[i][j] *= -1;
+                        fileOutput.Add("($");
+                        fileOutput[lineCounter] += Math.Round(sectionTotals[i][j], 2, MidpointRounding.AwayFromZero) + ")";
+                        lineCounter++;
+                        continue;
+                    }
+
+                  else
+                    {
+                        fileOutput.Add("$");
+                        fileOutput[lineCounter] += Math.Round(sectionTotals[i][j], 2, MidpointRounding.AwayFromZero);
+                        lineCounter++;
+                        continue;
+                    }
+                    
+                }
+
+                if (i != sectionTotals.Count() - 1)
+                {
+                    fileOutput.Add("");
+                    lineCounter++;
+                }
+
+            }
+
+
+            using (StreamWriter outputFile = new StreamWriter(fileName + ".out", true))
+            {
+
+                for (int i = 0; i < fileOutput.Count(); i++)
+                {
+                    outputFile.WriteLine(fileOutput[i]);
+                }
+            }
+
+        }
+
+       
+    }
+}
---
+++ CorityCodingScenario/CorityCodingScenarioTest/SplittingBillTest.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using CorityCodingScenario;
+
+/// <summary>
+/// This namespace consists of one class that is meant to test the SplittingBill class within the CorityCodingScenario namespace
+/// </summary>
+namespace CorityCodingScenarioTest
+{
+    /// <summary>
+    /// This test class is made to test different scenarios of the SplittingBill class
+    /// </summary>
+    [TestClass]
+    public class SplittingBillTest
+    {
+
+        SplittingBill splittingBill;
+
+        /// <summary>
+        /// Every test method needs an object of SplittingBill, so it will be initialized here before every test method
+        /// </summary>
+        [TestInitialize]
+        public void Initialize()
+        {
+            splittingBill = new SplittingBill();
+        }
+
+        /// <summary>
+        /// This test method will test to see if the SplitingBill class outputs the expected values
+        /// This will cover every path within the SplittingBill class (aside from main)
+        /// This tests both the ReadInput method and WriteOutput method within SplittingBill class
+        /// This test passes when the code works as expected
+        /// </summary>
+        [TestMethod]
+        public void StandardInputTest()
+        {
+
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses.txt");
+
+            splittingBill.ReadInput(fileName);
+            List<String> expectedOutput = new List<String>();
+            List<String> actualOutput = ReadOutput(fileName);
+            expectedOutput.Add("($1.99)");
+            expectedOutput.Add("($8.01)");
+            expectedOutput.Add("$10.01");
+            expectedOutput.Add("");
+            expectedOutput.Add("$0.98");
+            expectedOutput.Add("($0.98)");
+
+            CollectionAssert.AreEqual(expectedOutput, actualOutput);
+
+        }
+
+        /// <summary>
+        /// Typically for a scenario such as this looping till EndOfStream would suffice
+        /// However, as part of the requirements the last activity must be followed by a 0
+        /// This test is assuming that the 0 is mandatory
+        /// So, it will fail when the file does not have a 0 at the end, since in ReadInput the while loop can only stop at a 0
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.NullReferenceException), "Object reference not set to an instance of an object")]
+        public void NoneZeroEndTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\none-zero.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of there being a letter within the input file
+        /// It tests the integer portions of the input file
+        /// In case of something like this the code is expected to fail, since at some point a decimal will be attempted to be parsed as an int
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void LetterAtIntegerTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-letter-integer.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of there being a letter within the input file
+        /// It tests the decimal portions of the input file
+        /// In case of something like this the code is expected to fail, since at some point a decimal will be attempted to be parsed as an int
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void LetterAtDecimalTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-letter-decimal.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of the file containing inputs that are in the wrong order
+        /// In case of something like this the code is expected to fail, since at some point a decimal will be attempted to be parsed as an int
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void WrongOrderTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-wrong-order.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of the number of people (n) value being wrong.
+        /// In case of something like this the code is expected to fail, since at some point a decimal will be attempted to be parsed as an int
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void WrongNValueTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-wrong-n.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of the number of payments (p) value being wrong
+        /// In case of something like this the code is expected to fail, since at some point a decimal will be attempted to be parsed as an int
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void WrongPValueTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-wrong-n.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+        /// <summary>
+        /// This test considers the possibility of there being an empty line within the input file
+        /// The code is designed to not have any empty lines
+        /// In case of something like this the code is expected to fail, since at some point the code will attempt to parse for int or decimal
+        /// This test passes if the code fails, since the code should not work under such conditions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(System.FormatException), "Input String was not in a correct format")]
+        public void EmptyLineTest()
+        {
+            string fileName = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName, @"Files\\expenses-empty-line.txt");
+
+            splittingBill.ReadInput(fileName);
+        }
+
+
+        /// <summary>
+        /// This method reads the output file and makes a list of every line of string within the file
+        /// </summary>
+        /// <param name="fileName">the name of the file to be read</param>
+        /// <returns>returns the list of every line of string within the file</returns>
+        public List<String> ReadOutput(string fileName)
+        {
+            List<String> outputValues = new List<string>();
+            string line;
+            StreamReader file = new StreamReader(fileName + ".out");
+
+            while (!file.EndOfStream)
+            {
+                line = file.ReadLine();
+                outputValues.Add(line);
+            }
+
+            return outputValues;
+        }
+    }
+}
